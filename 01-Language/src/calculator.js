e21a459d4dbb201b465b77566025dcfd7a82ec3c
tkmagesh <tkmagesh77@gmail.com>
2015-02-01 11:52:13 +0530

    Language Features

---
+++ 01-Language/src/calculator.js
@@ -0,0 +1,15 @@
+function add(x,y){
+	function parseArg(n){
+		if (Array.isArray(n)) return add.apply(this, n);
+		if (typeof n === "function") return parseArg(n());
+		return  isNaN(n) ? 0 : parseInt(n,10);	
+	}
+	return arguments.length <= 1 ? parseArg(arguments[0]) : parseArg(arguments[0]) + add([].slice.call(arguments,1));
+}
+
+/*
+	arguments -> array like object , "length",[]
+	this -> invocation context
+		As a method of an object -> this => object
+		As a function -> this => window
+*/
